d29b8319d0578549253ec698aaa8dd21632d7c78
Marampally Sathwika <sathwikareddy1805@gmail.com>
2025-12-13 20:42:07 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,18 @@
+#creating a calculator
+a=100389
+b=4959385
+
+print("addition of two numbers is :" ,a+b)
+print("Subtraction of two numbers is :", a-b)
+print("Multipication of two numbers is :", a*b)
+print("Division of twp numbers is:", a/b)
+print("Modulus of two numbers is :", a%b)
+print("floor division of two numbers is:" ,a//b)
+print("exponential of two numbers is:", a**b)
+
+print("the value of",a,"+",b,"is :",a+b)
+print("the value of",a,"+",b,"is :",a-b)
+print("the value of",a,"+",b,"is :",a*b)
+print("the value of",a,"+",b,"is :",a/b)
+print("the value of",a,"+",b,"is :",a%b)
+print("the value of",a,"+",b,"is :",a**b)
