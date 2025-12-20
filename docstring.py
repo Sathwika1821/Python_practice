dcfae11a5880ebe04e2b6f4be55137e6951b66c0
Marampally Sathwika <sathwikareddy1805@gmail.com>
2025-12-20 23:26:56 +0530

    strings

---
+++ docstring.py
@@ -0,0 +1,5 @@
+def square(n):
+    '''It gives the square value of n '''
+    print(n**2)
+square(5)
+print(square.__doc__)
---
+++ fstring.py
@@ -0,0 +1,11 @@
+letter="Hey my name is {} and I am from {}"
+country="INDIA"
+name="Sathwika"
+print(letter.format(name , country))
+print(f"Hey my name is {name} and I am from {country}")
+txt = "For only {price:.1f} dollars!"
+print(txt.format(price = 49.29842948294))
+price=49.999
+txt1=f"for only {price:1f}dollars"
+print(txt1)
+print(f"{9*8}")
