07854822d070bf6816cf29bd13235221c7a7e433
Marampally Sathwika <sathwikareddy1805@gmail.com>
2025-12-28 18:24:08 +0530

    Add files via upload

---
+++ conditional_statements/elif.py
@@ -0,0 +1,11 @@
+num=int(input("Enter the value of num :"))
+if(num<0):
+    print("It is a Negative number")
+elif(num==0):
+    print("It is zero")
+elif(num==18):
+    print("It is a special number")
+else:
+    print("It is a positive number")
+
+print("Iam Happy now")
---
+++ conditional_statements/nestedif.py
@@ -0,0 +1,12 @@
+num=18
+if(num<0):
+    print("It is a negitive number")
+elif(num>=10):
+    if(num==10):
+        print("The value is 10")
+    elif(num==11 and num==20):
+        print("The value lies between 11 and 20")
+    else:
+        print("The value is greater than 20")
+else:
+    print("It is a positive number")
