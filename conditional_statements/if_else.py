ec7f5cd01385d39b834ee6745f07618f91bd9f5e
Marampally Sathwika <sathwikareddy1805@gmail.com>
2025-12-28 18:22:45 +0530

    Add if-else age voting eligibility check

---
+++ conditional_statements/if_else.py
@@ -0,0 +1,9 @@
+# conditional statements
+#conditional operators are <,>,<=,>=,==,!=
+
+a=int(input("Enter your age :"))
+print("Your age is :",a)
+if(a>=18):
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
